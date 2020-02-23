4eacfd456ff7273f3dbb76e40d2a22903250fc70
donggu kang <kang154123@naver.com>
2020-02-24 01:03:39 +0900

    0.7 버전 배포 (https://pypi.org/project/ailab-api/0.7/)

--- build/lib/ailab_api/sendMsg.py
+++ build/lib/ailab_api/sendMsg.py
@@ -2,23 +2,35 @@ import json
 import requests
 
 
-def sendSms(text, phone, key):
+def send_sms(text, phone_number, api_key):
     headers = {'Content-Type': 'application/json; charset=utf-8'}
 
     temp_dict = dict()
     temp_dict['text'] = str(text)
-    temp_dict['phone'] = str(phone)
-    temp_dict['key'] = str(key)
+    temp_dict['phone'] = str(phone_number)
+    temp_dict['key'] = str(api_key)
     json_data = json.dumps(temp_dict)
 
     try:
         request = requests.post('http://aiapi.ourplan.kr:5000/sms', headers=headers, data=json_data)
+        return {'status': int(request.status_code), 'type': 'sms', 'text': str(text)}
 
-        if int(request.status_code) == 200:
-            return {'text': '[OK] : 200 - ' + str(text)}
-        elif int(request.status_code) == 500:
-            return {'text': '[ERROR] : 500 - Server Error'}
-        elif int(request.status_code) == 403:
-            return {'text': '[ERROR] : 403 - API Key Error'}
     except Exception as error:
-        return {'text': '[ERROR] : ' + str(error)}
+        return {'status': 'error', 'type': 'sms', 'text': str(error)}
+
+
+def send_lms(text, phone_number, api_key):
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+
+    temp_dict = dict()
+    temp_dict['text'] = str(text)
+    temp_dict['phone'] = str(phone_number)
+    temp_dict['key'] = str(api_key)
+    json_data = json.dumps(temp_dict)
+
+    try:
+        request = requests.post('http://aiapi.ourplan.kr:5000/lms', headers=headers, data=json_data)
+        return {'status': int(request.status_code), 'type': 'lms', 'text': str(text)}
+
+    except Exception as error:
+        return {'status': 'error', 'type': 'lms', 'text': str(error)}
