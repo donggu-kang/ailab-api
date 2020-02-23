bd658a4e8b86e9d55d165c2fd4c159296784d108
donggu kang <kang154123@naver.com>
2020-02-24 00:54:35 +0900

    LMS 기능 추가

--- ailab_api/sendMsg.py
+++ ailab_api/sendMsg.py
@@ -19,4 +19,18 @@ def send_sms(text, phone_number, api_key):
         return {'status': 'error', 'type': 'sms', 'text': str(error)}
 
 
+def send_lms(text, phone_number, api_key):
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
 
+    temp_dict = dict()
+    temp_dict['text'] = str(text)
+    temp_dict['phone'] = str(phone_number)
+    temp_dict['key'] = str(api_key)
+    json_data = json.dumps(temp_dict)
+
+    try:
+        request = requests.post('http://aiapi.ourplan.kr:5000/lms', headers=headers, data=json_data)
+        return {'status': int(request.status_code), 'type': 'lms', 'text': str(text)}
+
+    except Exception as error:
+        return {'status': 'error', 'type': 'lms', 'text': str(error)}
